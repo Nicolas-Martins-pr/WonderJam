20d68d458d0da6d977d7ddb87b15b8f80e09297e
Lucas <lucas.miras@live.fr>
2021-10-09 19:31:41 -0400

    Add random Obstacle generation ( mountain, Ruin, Tree)

--- WonderJam102021/Assets/Scripts/Controller.cs
+++ WonderJam102021/Assets/Scripts/Controller.cs
@@ -6,6 +6,10 @@ public class Controller : MonoBehaviour {
 
     public int m_side = 7; // must be odd
 
+    public int m_nbTrees = 10;
+    public int m_nbMountain = 10;
+    public int m_nbRuins = 10;
+    public int m_nbPortals = 2;
     public GameObject m_board;
     
     public Tile[] gameBoard;
@@ -20,6 +24,7 @@ public class Controller : MonoBehaviour {
 
     private void Start() {
         SetAllBoardTiles();
+        GenerateObstacleZone();
         SetAllNextTiles();
     }
 
@@ -58,6 +63,7 @@ public class Controller : MonoBehaviour {
             {
                 position = new Vector3(j-half,0,i);
                 newTile = Instantiate(prefabTileG,position,Quaternion.identity,this.m_board.transform);
+                
                 newTile.name = "TileG " + i + j;
                 Tile newTileVar = newTile.GetComponent<Tile>();
                 newTileVar.m_positionH = i+1;
@@ -65,25 +71,113 @@ public class Controller : MonoBehaviour {
                 // Instantiate()
                 if(j== half && i == 0)
                 {
+                    
                     newTile.name = "TileG_PlayerStart " + i + j;
                     newTileVar.setPlayer(true);
                     this.m_player.setTile(newTileVar);
 
                     
                 }
-                if(j== 0 && i ==this.m_side - 1)
+                else if(j== 0 && i ==this.m_side - 1)
                 {
                     newTile.name = "TileG_EndPlayer1 " + i + j;
                     newTileVar.m_endPlayer1 = true;
+                    newTileVar.setGateHell(true);
                 }
-                if (j== m_side-1 && i == m_side -1)
+                else if (j== m_side-1 && i == m_side -1)
                 {
                     newTile.name = "TileG_EndPlayer2 " + i + j;
                     newTileVar.m_endPlayer2 = true;
+                    newTileVar.setGateHeaven(true);
+                }
+                else
+                {
+                    RotateTile(newTile);
                 }
                 this.m_gameBoardI.Add(newTileVar);
             }
         }
+    }
+    void RotateTile(GameObject tile)
+    {
+        int nbRotation =(int) (Random.value * 4);
+        if (nbRotation == 0)
+        {
+            tile.transform.Rotate(new Vector3(0,90,0));
+        }
+        else if (nbRotation == 1)
+        {
+            tile.transform.Rotate(new Vector3(0,180,0));
+
+        }
+         else if (nbRotation == 2)
+        {
+            tile.transform.Rotate(new Vector3(0,270,0));
+
+        }
+
+    }
+     void GenerateObstacleZone()
+    {
+        int nbtree = this.m_nbTrees, nbmountain = this.m_nbMountain, nbruin = this.m_nbRuins;
+        int rand3 = (int)(Random.value * 3);
+
+        int row = (int) (Random.value * m_side), column = (int) (Random.value * m_side); 
+        while (nbtree != 0 || nbruin != 0 || nbmountain != 0)
+        {
+            row = (int) (Random.value * m_side);
+            column = (int) (Random.value * m_side); 
+
+            rand3 = (int)(Random.value * 3);
+            if ((column == m_side/2 && row == 0) || (column == 0 && row == m_side -1) || (column == m_side -1 && row == m_side -1) ){}
+            else
+            {
+                Tile tile = (m_gameBoardI[row*m_side + column]);
+                if (tile.isWalkable())
+                {
+                    if (rand3 == 0)
+                    {
+                        if (nbtree != 0)
+                        {
+                            tile.setTree(true);
+                            nbtree-=1;
+                        }
+                        else
+                        {
+                            rand3 = 1;
+                        }
+                        
+                    }
+                    if (rand3 == 1)
+                    {
+                        if (nbmountain != 0)
+                        {
+                            tile.setMountain(true);
+                            nbmountain-=1;
+                        }
+                        else
+                        {
+                            rand3 = 2;
+                        }
+                        
+                    }
+                    if (rand3 == 2)
+                    {
+                        if (nbruin != 0)
+                        {
+                            tile.setRuin(true);
+                            nbruin-=1;
+                        }
+                        else
+                        {
+                            rand3 = 0;
+                        }
+                    }
+                
+                }
+            }
+        }
+
     }
 
     void SetAllNextTiles()
@@ -116,6 +210,8 @@ public class Controller : MonoBehaviour {
             }
         }
     }
+
+   
     void ResetBeforeAction()
     {
         DesactiveAllTileSelectorIndicator();
