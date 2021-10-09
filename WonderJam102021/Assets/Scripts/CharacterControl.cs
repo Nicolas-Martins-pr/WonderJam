c233b10d904e091e8409f7a7ee23bd8013bc8615
Lucas <lucas.miras@live.fr>
2021-10-09 12:36:30 -0400

    add movement player (notWorking) + creation map and all list for selectors Indicators

--- WonderJam102021/Assets/Scripts/CharacterControl.cs
+++ WonderJam102021/Assets/Scripts/CharacterControl.cs
@@ -5,8 +5,13 @@ using UnityEngine.UI;
 
 public class CharacterControl : MonoBehaviour
 {
-    public GameObject m_tile; 
-    public GameObject m_character;
+    //Todo list : création de map avec rotation aléatoire des objects qui compose le terrain
+    public Tile m_tile; 
+    public GameObject m_player;
+
+    // public GameObject m_gameBoard;
+
+    private bool m_clicked = false;
 
 
     // Start is called before the first frame update
@@ -18,25 +23,38 @@ public class CharacterControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     #region Accessors
+
+    public bool getPlayerClicked()
+    {
+        return this.m_clicked;
+    }
          
+    public Tile GetTile()
+    {
+        return this.m_tile;
+    }
     #endregion
 
     #region Mutators
-         
+    public void setTile(Tile tile) //set current tile
+    {
+        this.m_tile = tile;
+    }
     #endregion
 
     void OnMouseUp() 
     {
-        Debug.Log("click");    
+        Debug.Log("click");  
+        this.m_clicked = true;  
     }
 
-    void MoveAtPointerSelection(GameObject tile)
+    public void MoveAtPointerSelection(Transform tile)
     {
-        m_character.transform.position = tile.transform.position;
+        m_player.transform.position = tile.position;
     }
 
 
